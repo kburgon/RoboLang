c9817a1f7e79634d936aef13fd653f69d3d150b5
Kevin Burgon <kevin.burgon@aggiemail.usu.edu>
2016-01-04 20:55:36 -0700

    Tweaked shell view

--- Shell/RoboShell.cpp
+++ Shell/RoboShell.cpp
@@ -9,16 +9,18 @@ int RoboShell::startPrompt()
 {
     Parser roboParse;
     std::vector<std::string> command;
+    int cmdNum = 0;
     do
     {
         command.erase(command.begin(), command.end());
-        std::cout << "[cmd]$ ";
+        std::cout << "[cmd-" << cmdNum << "]$ ";
         std::getline(std::cin, rawCommand);
         command = roboParse.parseString(rawCommand);
         for (auto indPar:command)
         {
             std::cout << "[" << indPar << "]" << std::endl;
         }
+        ++cmdNum;
     } while (command.front() != "exit");
     return 0;
 }
